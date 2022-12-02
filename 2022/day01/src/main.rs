01da5920444d47283f9881a7788b6720e900797d
Tyler Coare <tyler.coare@gmail.com>
2022-12-02 12:12:17 +0000

    day 1

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,3 +1,50 @@
+fn part1() {
+    let numbers = include_str!("input.txt")
+        .lines();
+
+    let mut calories: Vec<i32> = vec![];
+    let mut count: i32 = 0;
+    
+    for cal in numbers {
+        if cal.is_empty() {
+            // empty line is break in elves
+            calories.push(count);
+            count = 0
+        } else {
+            count += cal.parse::<i32>().unwrap();
+        }
+    }
+    calories.sort();
+    calories.reverse();
+    println!("{:?}", calories[0]);
+}
+
+fn part2() {
+    let numbers = include_str!("input.txt")
+        .lines();
+
+    let mut calories: Vec<i32> = vec![];
+    let mut count: i32 = 0;
+    
+    for cal in numbers {
+        if cal.is_empty() {
+            // empty line is break in elves
+            calories.push(count);
+            count = 0
+        } else {
+            count += cal.parse::<i32>().unwrap();
+        }
+    }
+    calories.sort();
+    calories.reverse();
+    count = 0;
+    for i in 0..3 {
+        count += calories[i];
+    }
+    println!("{:?}", count)
+}
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
 }
