ba273ba6ad0daebb94e7550b1074cf7569759bb6
Tyler Coare <tyler.coare@gmail.com>
2022-12-07 13:37:32 +0000

    day 3 part 2

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
 
 fn part1() {
-    let input = include_str!("input.txt")
-        .lines();
+    let input = include_str!("input.txt").lines();
 
     let mut prio_value: HashMap<char, i32> = HashMap::new();
     let letters: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
@@ -15,21 +14,44 @@ fn part1() {
 
     let mut prio: i32 = 0;
     for line in input {
-        let (first, last) = line.split_at(line.len()/2);
+        let (first, last) = line.split_at(line.len() / 2);
 
         let mut seen: Vec<char> = vec![];
         for c in first.chars() {
             if last.contains(c) && !seen.contains(&c) {
-                prio += prio_value.get(&c).unwrap(); 
+                prio += prio_value.get(&c).unwrap();
                 seen.push(c)
             }
         }
-
     }
     println!("{:?}", prio)
+}
 
+fn part2() {
+    let input = include_str!("input.txt").lines();
+
+    let mut prio_value: HashMap<char, i32> = HashMap::new();
+    let letters: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
+    let mut count: i32 = 1;
+    for c in letters.chars() {
+        prio_value.insert(c, count);
+        count += 1
+    }
+
+    let mut prio: i32 = 0;
+    for group in input.collect::<Vec<&str>>().chunks(3) {
+        let mut seen: Vec<char> = vec![];
+        for c in group[0].chars() {
+            if group[1].contains(c) && group[2].contains(c) && !seen.contains(&c) {
+                prio += prio_value.get(&c).unwrap();
+                seen.push(c)
+            }
+        }
+    }
+    println!("{:?}", prio)
 }
 
 fn main() {
     part1();
+    part2();
 }
